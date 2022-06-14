581ed6638b9b5b02c6ea22cdd47fdb2b17d3ad5d
liujinye <liujinye@xiaomi.com>
2022-06-14 23:09:10 +0800

    freetype/fopen: Add CLOEXCE for ft_open.
    Signed-off-by: huxiandong <huxiandong@xiaomi.com>

--- src/sdf/rules.mk
+++ src/sdf/rules.mk
@@ -3,7 +3,7 @@
 #
 
 
-# Copyright (C) 2020-2022 by
+# Copyright (C) 2020-2021 by
 # David Turner, Robert Wilhelm, and Werner Lemberg.
 #
 # This file is part of the FreeType project, and may only be used, modified,
@@ -28,10 +28,9 @@ SDF_COMPILE := $(CC) $(ANSIFLAGS)                            \
 
 # sdf driver sources (i.e., C files)
 #
-SDF_DRV_SRC := $(SDF_DIR)/ftsdfrend.c   \
-               $(SDF_DIR)/ftsdf.c       \
-               $(SDF_DIR)/ftbsdf.c      \
-               $(SDF_DIR)/ftsdfcommon.c
+SDF_DRV_SRC := $(SDF_DIR)/ftsdfrend.c \
+               $(SDF_DIR)/ftsdf.c     \
+               $(SDF_DIR)/ftbsdf.c
 
 
 # sdf driver headers
