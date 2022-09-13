fbd49d9b883627a3da209981094cdea8185a8e6c
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-09-13 17:11:48 +0800

    Revert "Merge commit '8e68439a6ffc9e489a70f2c278a016fe15394abf' into dev"
    This reverts commit 2968d382b1f3a73c8bdf257550526be1071373c3, reversing
    changes made to f758492642b618d850e7a359ad220749556e7490.

--- src/sdf/ftsdf.c
+++ src/sdf/ftsdf.c
@@ -4,7 +4,7 @@
  *
  *   Signed Distance Field support for outline fonts (body).
  *
- * Copyright (C) 2020-2022 by
+ * Copyright (C) 2020-2021 by
  * David Turner, Robert Wilhelm, and Werner Lemberg.
  *
  * Written by Anuj Verma.
@@ -22,7 +22,6 @@
 #include <freetype/internal/ftdebug.h>
 #include <freetype/ftoutln.h>
 #include <freetype/fttrigon.h>
-#include <freetype/ftbitmap.h>
 #include "ftsdf.h"
 
 #include "ftsdferrs.h"
@@ -41,8 +40,7 @@
    *     file `ftbsdf.c` for more.
    *
    *   * The basic idea of generating the SDF is taken from Viktor Chlumsky's
-   *     research paper.  The paper explains both single and multi-channel
-   *     SDF, however, this implementation only generates single-channel SDF.
+   *     research paper.
    *
    *       Chlumsky, Viktor: Shape Decomposition for Multi-channel Distance
    *       Fields.  Master's thesis.  Czech Technical University in Prague,
@@ -204,7 +202,7 @@
   {
     FT_Memory  memory;
 
-  } SDF_TRaster, *SDF_PRaster;
+  } SDF_TRaster;
 
 
   /**************************************************************************
@@ -497,7 +495,7 @@
       goto Exit;
     }
 
-    if ( !FT_QNEW( ptr ) )
+    if ( !FT_ALLOC( ptr, sizeof ( *ptr ) ) )
     {
       *ptr = null_edge;
       *edge = ptr;
@@ -536,7 +534,7 @@
       goto Exit;
     }
 
-    if ( !FT_QNEW( ptr ) )
+    if ( !FT_ALLOC( ptr, sizeof ( *ptr ) ) )
     {
       *ptr     = null_contour;
       *contour = ptr;
@@ -591,7 +589,7 @@
       goto Exit;
     }
 
-    if ( !FT_QNEW( ptr ) )
+    if ( !FT_ALLOC( ptr, sizeof ( *ptr ) ) )
     {
       *ptr        = null_shape;
       ptr->memory = memory;
@@ -738,18 +736,6 @@
 
     contour = shape->contours;
 
-    /* If the control point coincides with any of the end points */
-    /* then it is a line and should be treated as one to avoid   */
-    /* unnecessary complexity later in the algorithm.            */
-    if ( ( contour->last_pos.x == control_1->x &&
-           contour->last_pos.y == control_1->y ) ||
-         ( control_1->x == to->x &&
-           control_1->y == to->y )               )
-    {
-      sdf_line_to( to, user );
-      goto Exit;
-    }
-
     FT_CALL( sdf_edge_new( memory, &edge ) );
 
     edge->edge_type = SDF_EDGE_CONIC;
@@ -776,9 +762,9 @@
                 const FT_26D6_Vec*  to,
                 void*               user )
   {
-    SDF_Shape*    shape   = ( SDF_Shape* )user;
-    SDF_Edge*     edge    = NULL;
-    SDF_Contour*  contour = NULL;
+    SDF_Shape*    shape    = ( SDF_Shape* )user;
+    SDF_Edge*     edge     = NULL;
+    SDF_Contour*  contour  = NULL;
 
     FT_Error   error  = FT_Err_Ok;
     FT_Memory  memory = shape->memory;
@@ -853,12 +839,12 @@
    *
    */
 
-  /* Return the control box of an edge.  The control box is a rectangle */
-  /* in which all the control points can fit tightly.                   */
+  /* Return the control box of a edge.  The control box is a rectangle */
+  /* in which all the control points can fit tightly.                  */
   static FT_CBox
   get_control_box( SDF_Edge  edge )
   {
-    FT_CBox  cbox   = { 0, 0, 0, 0 };
+    FT_CBox  cbox;
     FT_Bool  is_set = 0;
 
 
@@ -1077,7 +1063,7 @@
   static FT_Error
   split_sdf_conic( FT_Memory     memory,
                    FT_26D6_Vec*  control_points,
-                   FT_UInt       max_splits,
+                   FT_Int        max_splits,
                    SDF_Edge**    out )
   {
     FT_Error     error = FT_Err_Ok;
@@ -1146,40 +1132,25 @@
   static FT_Error
   split_sdf_cubic( FT_Memory     memory,
                    FT_26D6_Vec*  control_points,
-                   FT_UInt       max_splits,
+                   FT_Int        max_splits,
                    SDF_Edge**    out )
   {
-    FT_Error       error = FT_Err_Ok;
-    FT_26D6_Vec    cpos[7];
-    SDF_Edge*      left, *right;
-    const FT_26D6  threshold = ONE_PIXEL / 4;
+    FT_Error     error = FT_Err_Ok;
+    FT_26D6_Vec  cpos[7];
+    SDF_Edge*    left,*  right;
 
 
-    if ( !memory || !out )
+    if ( !memory || !out  )
     {
       error = FT_THROW( Invalid_Argument );
       goto Exit;
     }
 
-    /* split the cubic */
+    /* split the conic */
     cpos[0] = control_points[0];
     cpos[1] = control_points[1];
     cpos[2] = control_points[2];
     cpos[3] = control_points[3];
-
-    /* If the segment is flat enough we won't get any benefit by */
-    /* splitting it further, so we can just stop splitting.      */
-    /*                                                           */
-    /* Check the deviation of the Bezier curve and stop if it is */
-    /* smaller than the pre-defined `threshold` value.           */
-    if ( FT_ABS( 2 * cpos[0].x - 3 * cpos[1].x + cpos[3].x ) < threshold &&
-         FT_ABS( 2 * cpos[0].y - 3 * cpos[1].y + cpos[3].y ) < threshold &&
-         FT_ABS( cpos[0].x - 3 * cpos[2].x + 2 * cpos[3].x ) < threshold &&
-         FT_ABS( cpos[0].y - 3 * cpos[2].y + 2 * cpos[3].y ) < threshold )
-    {
-      split_cubic( cpos );
-      goto Append;
-    }
 
     split_cubic( cpos );
 
@@ -1277,32 +1248,13 @@
           /* Subdivide the curve and add it to the list. */
           {
             FT_26D6_Vec  ctrls[3];
-            FT_26D6      dx, dy;
-            FT_UInt      num_splits;
 
 
             ctrls[0] = edge->start_pos;
             ctrls[1] = edge->control_a;
             ctrls[2] = edge->end_pos;
 
-            dx = FT_ABS( ctrls[2].x + ctrls[0].x - 2 * ctrls[1].x );
-            dy = FT_ABS( ctrls[2].y + ctrls[0].y - 2 * ctrls[1].y );
-            if ( dx < dy )
-              dx = dy;
-
-            /* Calculate the number of necessary bisections.  Each      */
-            /* bisection causes a four-fold reduction of the deviation, */
-            /* hence we bisect the Bezier curve until the deviation     */
-            /* becomes less than 1/8th of a pixel.  For more details    */
-            /* check file `ftgrays.c`.                                  */
-            num_splits = 1;
-            while ( dx > ONE_PIXEL / 8 )
-            {
-              dx         >>= 2;
-              num_splits <<= 1;
-            }
-
-            error = split_sdf_conic( memory, ctrls, num_splits, &new_edges );
+            error = split_sdf_conic( memory, ctrls, 32, &new_edges );
           }
           break;
 
@@ -1323,10 +1275,8 @@
 
         default:
           error = FT_THROW( Invalid_Argument );
-        }
-
-        if ( error != FT_Err_Ok )
           goto Exit;
+        }
 
         edges = edges->next;
       }
@@ -1939,7 +1889,7 @@
     /* now factor is 16.16 */
     factor = FT_DivFix( factor, sq_line_length );
 
-    /* clamp the factor between 0.0 and 1.0 in fixed-point */
+    /* clamp the factor between 0.0 and 1.0 in fixed point */
     if ( factor > FT_INT_16D16( 1 ) )
       factor = FT_INT_16D16( 1 );
     if ( factor < 0 )
@@ -2947,10 +2897,6 @@
   /* `sdf_generate' is not used at the moment */
 #if 0
 
-  #error "DO NOT USE THIS!"
-  #error "The function still outputs 16-bit data, which might cause memory"
-  #error "corruption.  If required I will add this later."
-
   /**************************************************************************
    *
    * @Function:
@@ -3014,7 +2960,7 @@
         diff = current_dist.distance - min_dist.distance;
 
 
-        if ( FT_ABS( diff ) < CORNER_CHECK_EPSILON )
+        if ( FT_ABS(diff ) < CORNER_CHECK_EPSILON )
           min_dist = resolve_corner( min_dist, current_dist );
         else if ( diff < 0 )
           min_dist = current_dist;
@@ -3164,7 +3110,7 @@
         if ( min_dist.distance > sp_sq )
           min_dist.distance = sp_sq;
 
-        /* square_root the values and fit in a 6.10 fixed-point */
+        /* square_root the values and fit in a 6.10 fixed point */
         if ( USE_SQUARED_DISTANCES )
           min_dist.distance = square_root( min_dist.distance );
 
@@ -3246,8 +3192,8 @@
     FT_Int  width, rows, i, j;
     FT_Int  sp_sq;            /* max value to check   */
 
-    SDF_Contour*   contours;  /* list of all contours */
-    FT_SDFFormat*  buffer;    /* the bitmap buffer    */
+    SDF_Contour*  contours;   /* list of all contours */
+    FT_Short*     buffer;     /* the bitmap buffer    */
 
     /* This buffer has the same size in indices as the    */
     /* bitmap buffer.  When we check a pixel position for */
@@ -3255,8 +3201,6 @@
     /* This way we can find out which pixel is set,       */
     /* and also determine the signs properly.             */
     SDF_Signed_Distance*  dists = NULL;
-
-    const FT_16D16  fixed_spread = FT_INT_16D16( spread );
 
 
     if ( !shape || !bitmap )
@@ -3278,19 +3222,18 @@
       goto Exit;
     }
 
-    if ( FT_ALLOC( dists,
-                   bitmap->width * bitmap->rows * sizeof ( *dists ) ) )
-      goto Exit;
-
     contours = shape->contours;
     width    = (FT_Int)bitmap->width;
     rows     = (FT_Int)bitmap->rows;
-    buffer   = (FT_SDFFormat*)bitmap->buffer;
+    buffer   = (FT_Short*)bitmap->buffer;
+
+    if ( FT_ALLOC( dists, width * rows * sizeof ( *dists ) ) )
+      goto Exit;
 
     if ( USE_SQUARED_DISTANCES )
-      sp_sq = FT_INT_16D16( (FT_Int)( spread * spread ) );
+      sp_sq = FT_INT_16D16( spread * spread );
     else
-      sp_sq = fixed_spread;
+      sp_sq = FT_INT_16D16( spread );
 
     if ( width == 0 || rows == 0 )
     {
@@ -3332,7 +3275,6 @@
             FT_26D6_Vec          grid_point = zero_vector;
             SDF_Signed_Distance  dist       = max_sdf;
             FT_UInt              index      = 0;
-            FT_16D16             diff       = 0;
 
 
             if ( x < 0 || x >= width )
@@ -3360,27 +3302,23 @@
             if ( dist.distance > sp_sq )
               continue;
 
-            /* take the square root of the distance if required */
+            /* square_root the values and fit in a 6.10 fixed-point */
             if ( USE_SQUARED_DISTANCES )
               dist.distance = square_root( dist.distance );
 
             if ( internal_params.flip_y )
-              index = (FT_UInt)( y * width + x );
+              index = y * width + x;
             else
-              index = (FT_UInt)( ( rows - y - 1 ) * width + x );
+              index = ( rows - y - 1 ) * width + x;
 
             /* check whether the pixel is set or not */
             if ( dists[index].sign == 0 )
               dists[index] = dist;
-            else
-            {
-              diff = FT_ABS( dists[index].distance - dist.distance );
-
-              if ( diff <= CORNER_CHECK_EPSILON )
-                dists[index] = resolve_corner( dists[index], dist );
-              else if ( dists[index].distance > dist.distance )
-                dists[index] = dist;
-            }
+            else if ( dists[index].distance > dist.distance )
+              dists[index] = dist;
+            else if ( FT_ABS( dists[index].distance - dist.distance )
+                        < CORNER_CHECK_EPSILON )
+              dists[index] = resolve_corner( dists[index], dist );
           }
         }
 
@@ -3403,26 +3341,26 @@
 
       for ( i = 0; i < width; i++ )
       {
-        index = (FT_UInt)( j * width + i );
+        index = j * width + i;
 
         /* if the pixel is not set                     */
         /* its shortest distance is more than `spread` */
         if ( dists[index].sign == 0 )
-          dists[index].distance = fixed_spread;
+          dists[index].distance = FT_INT_16D16( spread );
         else
           current_sign = dists[index].sign;
 
         /* clamp the values */
-        if ( dists[index].distance > fixed_spread )
-          dists[index].distance = fixed_spread;
+        if ( dists[index].distance > (FT_Int)FT_INT_16D16( spread ) )
+          dists[index].distance = FT_INT_16D16( spread );
 
-        /* flip sign if required */
-        dists[index].distance *= internal_params.flip_sign ? -current_sign
-                                                           :  current_sign;
+        /* convert from 16.16 to 6.10 */
+        dists[index].distance /= 64;
 
-        /* concatenate to appropriate format */
-        buffer[index] = map_fixed_to_sdf( dists[index].distance,
-                                          fixed_spread );
+        if ( internal_params.flip_sign )
+          buffer[index] = (FT_Short)dists[index].distance * -current_sign;
+        else
+          buffer[index] = (FT_Short)dists[index].distance * current_sign;
       }
     }
 
@@ -3559,9 +3497,9 @@
     SDF_Contour*  head;              /* head of the contour list      */
     SDF_Shape     temp_shape;        /* temporary shape               */
 
-    FT_Memory      memory;           /* to allocate memory            */
-    FT_SDFFormat*  t;                /* target bitmap buffer          */
-    FT_Bool        flip_sign;        /* flip sign?                    */
+    FT_Memory  memory;               /* to allocate memory            */
+    FT_6D10*   t;                    /* target bitmap buffer          */
+    FT_Bool    flip_sign;            /* filp sign?                    */
 
     /* orientation of all the separate contours */
     SDF_Contour_Orientation*  orientations;
@@ -3573,11 +3511,6 @@
 
     if ( !shape || !bitmap || !shape->memory )
       return FT_THROW( Invalid_Argument );
-
-    /* Disable `flip_sign` to avoid extra complication */
-    /* during the combination phase.                   */
-    flip_sign                 = internal_params.flip_sign;
-    internal_params.flip_sign = 0;
 
     contour           = shape->contours;
     memory            = shape->memory;
@@ -3594,14 +3527,17 @@
     }
 
     /* allocate the bitmaps to generate SDF for separate contours */
-    if ( FT_ALLOC( bitmaps,
-                   (FT_UInt)num_contours * sizeof ( *bitmaps ) ) )
+    if ( FT_ALLOC( bitmaps, num_contours * sizeof ( *bitmaps ) ) )
       goto Exit;
 
     /* allocate array to hold orientation for all contours */
-    if ( FT_ALLOC( orientations,
-                   (FT_UInt)num_contours * sizeof ( *orientations ) ) )
+    if ( FT_ALLOC( orientations, num_contours * sizeof ( *orientations ) ) )
       goto Exit;
+
+    /* Disable `flip_sign` to avoid extra complication */
+    /* during the combination phase.                   */
+    flip_sign                 = internal_params.flip_sign;
+    internal_params.flip_sign = 0;
 
     contour = shape->contours;
 
@@ -3618,8 +3554,7 @@
       bitmaps[i].pixel_mode = bitmap->pixel_mode;
 
       /* allocate memory for the buffer */
-      if ( FT_ALLOC( bitmaps[i].buffer,
-                     bitmap->rows * (FT_UInt)bitmap->pitch ) )
+      if ( FT_ALLOC( bitmaps[i].buffer, bitmap->rows * bitmap->pitch ) )
         goto Exit;
 
       /* determine the orientation */
@@ -3682,7 +3617,7 @@
     shape->contours = head;
 
     /* cast the output bitmap buffer */
-    t = (FT_SDFFormat*)bitmap->buffer;
+    t = (FT_6D10*)bitmap->buffer;
 
     /* Iterate over all pixels and combine all separate    */
     /* contours.  These are the rules for combining:       */
@@ -3697,18 +3632,18 @@
     {
       for ( i = 0; i < width; i++ )
       {
-        FT_Int  id = j * width + i;       /* index of current pixel    */
-        FT_Int  c;                        /* contour iterator          */
+        FT_Int   id = j * width + i;         /* index of current pixel    */
+        FT_Int   c;                          /* contour iterator          */
 
-        FT_SDFFormat  val_c  = 0;         /* max clockwise value       */
-        FT_SDFFormat  val_ac = UCHAR_MAX; /* min counter-clockwise val */
+        FT_6D10  val_c  = SHRT_MIN;          /* max clockwise value       */
+        FT_6D10  val_ac = SHRT_MAX;          /* min counter-clockwise val */
 
 
         /* iterate through all the contours */
         for ( c = 0; c < num_contours; c++ )
         {
           /* current contour value */
-          FT_SDFFormat  temp = ( (FT_SDFFormat*)bitmaps[c].buffer )[id];
+          FT_6D10  temp = ((FT_6D10*)bitmaps[c].buffer)[id];
 
 
           if ( orientations[c] == SDF_ORIENTATION_CW )
@@ -3719,10 +3654,7 @@
 
         /* Finally find the smaller of the two and assign to output. */
         /* Also apply `flip_sign` if set.                            */
-        t[id] = FT_MIN( val_c, val_ac );
-
-        if ( flip_sign )
-          t[id] = invert_sign( t[id] );
+        t[id] = FT_MIN( val_c, val_ac ) * ( flip_sign ? -1 : 1 );
       }
     }
 
@@ -3745,9 +3677,6 @@
       }
     }
 
-    /* restore the `flip_sign` property */
-    internal_params.flip_sign = flip_sign;
-
     return error;
   }
 
@@ -3759,19 +3688,25 @@
    */
 
   static FT_Error
-  sdf_raster_new( FT_Memory     memory,
-                  SDF_PRaster*  araster )
+  sdf_raster_new( FT_Memory   memory,
+                  FT_Raster*  araster)
   {
-    FT_Error     error;
-    SDF_PRaster  raster = NULL;
+    FT_Error      error  = FT_Err_Ok;
+    SDF_TRaster*  raster = NULL;
+    FT_Int        line   = __LINE__;
+
+    /* in non-debugging mode this is not used */
+    FT_UNUSED( line );
 
 
-    if ( !FT_NEW( raster ) )
+    *araster = 0;
+    if ( !FT_ALLOC( raster, sizeof ( SDF_TRaster ) ) )
+    {
       raster->memory = memory;
+      *araster       = (FT_Raster)raster;
+    }
 
-    *araster = raster;
-
-   return error;
+    return error;
   }
 
 
